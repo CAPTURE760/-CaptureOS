0c4f39c82fd1bbda4d1824cd100554045ebba29d
oywy <2935151401@qq.com>
2026-06-15 14:34:31 +0800

    fix: 简化 App.vue，移除 Naive UI 包裹

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,3 @@
 <template>
-  <n-config-provider :theme="darkTheme">
-    <n-message-provider>
-      <n-dialog-provider>
-        <router-view />
-      </n-dialog-provider>
-    </n-message-provider>
-  </n-config-provider>
+  <router-view />
 </template>
-
-<script setup lang="ts">
-import { NConfigProvider, NMessageProvider, NDialogProvider, darkTheme } from 'naive-ui'
-</script>
